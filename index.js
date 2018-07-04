c436c8e68f3d7c79218866950771e45a7c8f7dd7
AspenJames <aspenjames@tqca.org>
2018-07-03 18:47:26 -0700

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(nameArr) {
+  for (var i = 0; i < nameArr.length; i++) {
+    console.log(`Welcome ${nameArr[i]}! You are employee #${i+1}.`);
+  }
+  return nameArr;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails ++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
